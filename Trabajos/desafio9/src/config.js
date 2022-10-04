9e021f45c082d2ebfcac1f62c0f741c121d9b6eb
PatricioVeliz <velizpatricio02@gmail.com>
2022-10-04 20:23:47 -0300

    entrega9

---
+++ Trabajos/desafio9/src/config.js
@@ -0,0 +1,27 @@
+export default {
+    mongodb: {
+        cnxStr: 'mongodb+srv://PatricioVeliz32065:UJSDX1og17OeCl4A@cluster0.x4mqe7h.mongodb.net/?retryWrites=true&w=majority',
+        options: {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useCreateIndex: true,
+            serverSelectionTimeoutMS: 5000,
+        }
+    },
+    sqlite3: {
+        client: 'sqlite3',
+        connection: {
+            filename: `./DB/ecommerce.sqlite`
+        },
+        useNullAsDefault: true
+    },
+    mariaDb: {
+        client: 'mysql',
+        connection: {
+            host: 'localhost',
+            user: 'root',
+            password: '',
+            database: 'coderhouse'
+        }
+    }
+}
